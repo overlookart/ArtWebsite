46afc3af27cb2a210a6a8e7d3523228f9b67e3e6
xzh <>
2021-05-31 16:53:09 +0800

    左侧菜单导航

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,37 @@
 <template>
     <div id="app">
-        <h1>APP</h1>
+        <el-container>
+            <el-aside width="200px">
+                <nav-menu></nav-menu>
+            </el-aside>
+        </el-container>
     </div>
 </template>
 
 <script>
 
+const NavMenu = () => import("../src/views/NavMenu")
 export default {
     name: "app",
     components: {
-
+        NavMenu
     }
 }
 </script>
 
 <style>
+.el-header {
+    background-color: #B3C0D1
+}
 
+.el-aside {
+    background-color: cadetblue;
+    text-align: center;
+    line-height: 60px;
+}
+
+.el-main {
+    text-align: center;
+    background-color: darkseagreen;
+}
 </style>
---
+++ src/views/NavMenu.vue
@@ -0,0 +1,83 @@
+<!--
+* @Author: xzh
+* @Date: 2021-05-21 00:31:25
+* @LastEditTime: 2021-05-21 10:09:32
+* @LastEditors: xzh
+* @Description: 组件描述
+* @FilePath: /文件路径.vue
+* 代码版权方：'政府采购信息网'
+-->
+<template>
+ <div class="">
+     <el-menu 
+     mode="vertical"
+     background-color="#909399"
+     text-color="#303133"
+     active-text-color="#409EFF"
+     default-active="1"
+     @open="openMenuAction"
+     @close="closeMenuAction"
+     >
+       <el-submenu index="1">
+            <span slot="title">Menu</span>
+            <el-menu-item index="1-1"></el-menu-item>
+            <el-menu-item index="1-2"></el-menu-item>
+       </el-submenu>
+       <el-submenu index="2">
+            <span slot="title">ksbe</span>
+            <el-menu-item index="1-1"></el-menu-item>
+            <el-menu-item index="1-2"></el-menu-item>
+       </el-submenu>
+     </el-menu>
+ </div>
+</template>
+ 
+<script>
+// 这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+// 例如：import 《组件名称》 from '《组件路径》';
+export default {
+    components: {},
+    data () {
+      return {
+        menus: [
+
+        ]
+      }
+    },
+// 监听属性 类似于data概念
+    computed: {}, 
+// 监控data中的数据变化
+    watch: {},
+// 方法集合
+    methods: {
+        openMenuAction(key, keyPath){
+            console.log("打开菜单",key, keyPath);
+        },
+
+        closeMenuAction(key, keyPath){
+            console.log("关闭菜单",key, keyPath);
+        }
+    },
+/*------生命周期-------*/
+//创建完成（可以访问当前this实例）
+    created() {},
+//挂载完成（可以访问DOM元素）
+    mounted() {},
+//创建之前
+    beforeCreate() {},
+//挂载之前
+    beforeMount() {},
+//更新之前
+    updated() {},
+//销毁之前
+    beforeDestroy() {},
+//销毁完成
+    destroyed() {},
+//如果页面有keep-alive缓存功能，这个函数会触发
+    activated() {},
+};
+</script>
+ 
+<style>
+
+</style>

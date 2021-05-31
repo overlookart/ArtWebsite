56253bdd85459f98f2f67d4edbadb93f746d8052
xzh <>
2021-05-31 18:06:28 +0800

    更新布局

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,12 @@
             <el-aside width="200px">
                 <nav-menu></nav-menu>
             </el-aside>
+            
+            <el-container>
+                <el-header>header</el-header>
+                <el-main>main</el-main>
+                <el-footer>footer</el-footer>
+            </el-container>
         </el-container>
     </div>
 </template>
@@ -33,5 +39,8 @@ export default {
 .el-main {
     text-align: center;
     background-color: darkseagreen;
+}
+#app {
+    width: 100%;
 }
 </style>

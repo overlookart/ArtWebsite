9d195f9ec1b9e84e65910eda7aa713e45310062e
xzh <18334786772@163.com>
2024-01-11 15:31:32 +0800

    集成 Vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://overlookart.github.io',
-    base: '/ArtWebsite'
+  site: 'https://overlookart.github.io',
+  base: '/ArtWebsite',
+  // 集成
+  integrations: [vue()]
 });

ceb8eb60b207a60a1875f0cdd9428dd426324b8b
xzh <18334786772@163.com>
2024-01-11 17:14:30 +0800

    集成 tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://overlookart.github.io',
   base: '/ArtWebsite',
   // 集成
-  integrations: [vue()]
+  integrations: [vue(), tailwind()]
 });

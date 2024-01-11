d94e9af2b0ed7a5fe3bf180dc89dfbcb92646a41
xzh <18334786772@163.com>
2024-01-11 10:01:34 +0800

    部署配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://overlookart.github.io',
+    base: '/ArtWebsite'
+});

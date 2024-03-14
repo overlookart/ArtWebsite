1d6a0d969f7612008d75ae62e678cb060ee111af
xzh <18334786772@163.com>
2024-03-14 17:12:29 +0800

    集成数据库

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
+import db from "@astrojs/db";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://overlookart.github.io',
   base: '/ArtWebsite',
   // 集成
-  integrations: [vue(), tailwind()]
+  integrations: [vue(), tailwind(), db()]
 });
